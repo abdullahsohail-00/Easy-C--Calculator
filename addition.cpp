069394cf43cb7f34158ee0c4f5e5fe8a49b613ce
M Abdullah Sohail <abdullahsohailas3@gmail.com>
2022-07-26 03:43:15 +0500

    Initial commit

---
+++ addition.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a;
+    int b;
+    int sum;
+
+    cout << "Enter the first integer" << endl;
+    cin >> a;
+    cout << "Enter the second integer" << endl;
+    cin >> b;
+
+    sum = a + b;
+
+    cout << "The sum is = " << sum;
+
+    return 0;
+}
